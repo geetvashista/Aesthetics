35f13c8c2539de0e9f91ac1ad26746683a73bfd2
Geet Vashista <148626773+geetvashista@users.noreply.github.com>
2025-09-05 12:12:25 +1200

    Update Plot _HCP_atlas_ graphs.py

--- Plot _HCP_atlas_ graphs.py
+++ Plot _HCP_atlas_ graphs.py
@@ -7,14 +7,27 @@ import netplotbrain
 import pandas as pd
 import numpy as np
 
-# Get loc
-array = np.load(r'')
+# # Get loc
+# array = np.load(r'')
 
-# Plotting
-nodes = pd.DataFrame(data={'x': (array[:,0] - 128),
-                           'y': (array[:,1] - 150),
-                           'z': (array[:,2] - 100),
-                           })
+# # Plotting
+# nodes = pd.DataFrame(data={'x': (array[:,0] - 128),
+#                            'y': (array[:,1] - 150),
+#                            'z': (array[:,2] - 100),
+#                            })
+
+# read the xyz file
+file_path = "HCP_coordinates.csv"    # TODO: change to the right path
+file_raw_data = pd.read_csv(file_path)
+
+xyz_dataframe = pd.DataFrame({
+    "x": list(file_raw_data["x-cog"]),
+    "y": list(file_raw_data["y-cog"]),
+    "z": list(file_raw_data["z-cog"]),
+    "communites": [], # list of sig nodes, needs to be len = len(xyz)
+})
+
+nodes = xyz_dataframe
 
 netplotbrain.plot(template='MNI152NLin2009cAsym',
                   nodes=nodes,
@@ -22,3 +35,4 @@ netplotbrain.plot(template='MNI152NLin2009cAsym',
                   view=['LSR'],
                   arrowaxis=None,
                   node_scale=100)
+
